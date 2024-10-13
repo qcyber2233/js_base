c0371b63009531253313dcffbc00a046bd962e58
Oleg kog <zidyhzli@acccmail.com>
2024-10-14 00:21:24 +0300

    1-50

---
+++ olegi_1-50.js
@@ -0,0 +1,196 @@
+"use strict";
+alert('text');
+
+
+
+
+
+let num = 123;
+alert(num);
+
+
+
+let a1 = 1, b1 = 2, c1 = 3;
+let a2, b2, c2; 
+a2 = 1;
+b2 = 2;
+c2 = 3;
+
+
+
+let a;    
+
+a = 10;    
+alert(a); // выведет 1
+
+a = 20;    
+alert(a); 
+
+
+let sum17 = 1+2+3;
+alert(sum17);
+
+
+let a18_a = 10;
+let a18_b = 2;
+let sumpe18_1 = a18_a + a18_b;
+let diff18_1 = a18_a - a18_b;
+let comp18_1 = a18_a * a18_b;
+let divis18_1 = a18_a / a18_b;
+
+alert('Сумма: ' + sumpe18_1 + ' ' +
+    'Разность: ' + diff18_1 + ' ' +
+    'Произведение: ' + comp18_1 + ' ' +
+    'Частное: ' + divis18_1);
+
+let a18_2a = 10;
+let a18_2d = 5;
+let result;
+alert(result = a18_2a + a18_2d);
+
+let a18_3a = 1;
+let a18_3b = 2;
+let a18_3c = 3;
+let sum18_3;
+alert(sum18_3 = a18_3a + a18_3b + a18_3c)
+
+let a18_4a = 10;
+let a18_4b = 5;
+let a18_4c = a18_4a - a18_4b;
+let a18_4d = 7;
+let a18_4result = a18_4d + a18_4c;
+alert(a18_4result);
+
+
+let a22_a = 1.5;
+let a22_b = 0.75;
+alert(a22_a+a22_b);
+
+
+let a23_a = -100;
+let a23_b = 1;
+alert('Отрицательное: ' + a23_a + ' ' +
+    'Положительное с - впереди: ' + -a23_b);
+
+    
+let a25_a = 13;
+let a25_b = 5;
+alert(a25_a % a25_b);
+
+
+let a26_a = 2;
+alert(a26_a ** 10);
+
+
+let a28_name_fname = 'Когутов Олег';
+alert(a28_name_fname);
+
+
+let a29_1 = '!!!';
+alert(a29_1);
+let a29_java = 'java';
+let a29_script = 'script';
+alert(a29_java + a29_script);
+let a29_hello = 'hello';
+let a29_world = 'world';
+alert(a29_hello + " " + a29_world);
+
+
+let a30_primer = 'olegiii';
+alert('Количество строк в переменной: ' + a30_primer.length);
+
+
+let str31_31 = 'xxx';
+let str32_31 = 'yyy';
+let txt_31  = `aaa ${str31_31} bbb ${str32_31} ccc`;
+alert(txt_31);
+
+
+let str_oleg_32 = `a
+b
+c
+`;
+alert(str_oleg_32);
+
+
+
+
+
+let a_34;
+alert(a_34);
+
+
+let a_35 = null;
+alert(a_35);
+
+
+let oleg_36_true = true;
+let oleg_36_false = false;
+alert(' значение true: ' + oleg_36_true
+    +'  значение false: ' + oleg_36_false);
+
+
+let oleg1_37 = 'abc';
+let oleg2_37 = 'def';
+alert(oleg1_37 * oleg2_37);
+
+
+
+let a_39_1 = 18;
+let [a_39_2a, a_39_2b, a_39_2c] = [1, 2, 3];
+console.log(a_39_1);
+console.log(a_39_2a, a_39_2b, a_39_2c);
+
+
+console.log(123);               // число
+console.log('Hello, world!');   // строка
+console.log(true);              // булевое
+console.log(null);              // Null
+console.log(undefined);         // Undefined
+console.log({});                // пустой объект
+console.log([]);                // массив
+
+
+const PI = 3.14;
+let oleg_42 = 2;
+let a_42 = PI * oleg_42 ** 2;
+alert(a_42);
+
+
+let a_44 = '10'
+let b_44 = '20'
+alert(Number(a_44) + Number(b_44));
+
+
+let a_45 = +'2';
+let b_45 = +'3';
+alert(a_45 + b_45); 
+
+
+let a47a1 = parseFloat('5px');
+let a47b1 = parseFloat('6px');
+alert(a47a1 + a47b1);
+
+let a47a2 = parseFloat('5.5px');
+let a47b2 = parseFloat('6.25px');
+alert(a47a2 + a47b2);
+
+let a47a3 = parseFloat('5.5px');
+let a47b3 = parseFloat('6.25px');
+alert(a47a3 + a47b3 + 'px');
+
+
+
+let a48a = 120;
+let a48b = 240;
+alert(String(a48a) + String(a48b));
+
+
+let a49 = 12345;
+alert(String(a49).length);
+
+let a49_a = 873;
+let a49_b = 4326;
+let str49_a = String(a49_a); 
+let str49_b = String(a49_b);
+alert(str49_a.length + str49_b.length);
